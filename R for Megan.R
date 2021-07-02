8d79d7dbc04f900ea1c142623b36bf3f73a70879
meganewing <66333757+meganewing@users.noreply.github.com>
2021-07-02 14:24:21 -0700

    Ran through script for muscle_c with no averages

--- R for Megan.R
+++ R for Megan.R
@@ -2,11 +2,8 @@
 
 #First we will make a project on Github and link it your Git
 
-<<<<<<< HEAD
 #R is finicky and can be annoying, but gets easier with time, particulary easier when you know what terms to google... #Nevertheless, she persisted...
-=======
 #R is finicky and can be annoying, but gets easier with time, particulary easier when you know what terms too google... #Nevertheless, she persisted...
->>>>>>> 42e2734a74cbd4eed2dc8991ea15f914a886c10f
 
 #In R, you'll need to download packages to use certain functions. Once the packages are downloaded you call them in with the library function. Look below for the names of packages <library(package)> and search for them using the install button on the right corner packages tab.
 
@@ -23,22 +20,10 @@ library(ggplot2)
 library(cowplot)
 
 #Okay, lets read in the files and packages we will use for data analysis. If the files is stored in your R project, all you need is a read.csv command! below you will see I am calling in the csv called lionfish data and from here out I'm calling this datasheet/dataframe "lion"
-lion<-read.csv("lionfishdata.csv", header=TRUE)
+lion<-read.csv("0702-lionfishdata.csv", header=TRUE)
 
 #check that the factors are properply called numeric or charachter; if they're not correct we can fix that.
 #lets just start with some descriptive statistics (mean, standard deviation, standard errror)
-
-library(tidyverse)
-library(dplyr)
-library(broom)
-library(DHARMa)
-library(spdep)
-library(glmmTMB)
-library(multcomp)
-library(lmtest)
-library(car)
-library(ggplot2)
-library(cowplot)
 
 #The "tidyverse" is one of the most succinct and user friendly ways to summarise and organize data. You can give it instructions for things like: do this, then that, by using that pipe feature which is denoted via %>%
 #This website https://r4ds.had.co.nz/index.html has amazing tutorials and references. You can also google tidy cheatsheet or your question and then tidy in r, and usually a helpful example pops up.
@@ -47,43 +32,52 @@ library(cowplot)
 
 #Here I am making a dataframe just for standards, and looking at the mean and sd of C and N
 standards<-lion%>%
-  filter(calibrate == "standard") %>% 
+  filter(calibration == "standard") %>% 
   summarize(
-    meansalmonC = mean(standard_c, na.rm = TRUE),
-    sdsamlmonC= sd(standard_c, na.rm = TRUE),
-    meansalmonN = mean(standard_n, na.rm = TRUE),
-    sdsamlmonN= sd(standard_n, na.rm = TRUE))
+    meansalmonC = mean(muscle_c, na.rm = TRUE),
+    sdsamlmonC= sd(muscle_c, na.rm = TRUE),
+    meansalmonN = mean(muscle_n, na.rm = TRUE),
+    sdsamlmonN= sd(muscle_n, na.rm = TRUE))
   
 #For our analyses we don't want to include salmon standards or duplicates, so we are going to remove them from the dataframe, and then proceed for the mean and sd for M, S, H, G for C and N
-lionstats<-lion%>%
-  filter(calibrate ==! "standard") %>%
-  filter (calibrate ==! "duplicate") %>%
-  group_by(depth, na.rm = TRUE)%>% 
+liondata<-lion %>% 
+  filter(calibration == "single" |calibration == "average")
+
+descriptivestats<-liondata %>% 
+  group_by(depth.categorical., na.rm = TRUE)%>% 
   summarize(
     meanmuscleC = mean(muscle_c,na.rm=TRUE),
     sdmuscleC = sd(muscle_c, na.rm = TRUE),
-    
-    #repeat this structure for the other tissue types and nitrogen and end with a closed parenthesis
-    
-  )
+    meanscaleC = mean(scale_c,na.rm=TRUE),
+    sdscaleC = sd(scale_c, na.rm = TRUE),
+    meanheartC = mean(heart_c,na.rm=TRUE),
+    sdheartC = sd(heart_c, na.rm = TRUE),
+    meanmuscleN = mean(muscle_n,na.rm=TRUE),
+    sdmuscleN = sd(muscle_n, na.rm = TRUE),
+    meanscaleN = mean(scale_n,na.rm=TRUE),
+    sdscaleN = sd(scale_n, na.rm = TRUE),
+    meanheartN = mean(heart_n,na.rm=TRUE),
+    sdheartB = sd(heart_n, na.rm = TRUE))
 
 #lets visualize our data by making a figure using the new dataframe we just created
-trophicfig<-ggplot(data = lionstats, aes(x = muscle_c,y = muscle_n)) + 
+trophicfig<-ggplot(data = descriptivestats, aes(x = meanmuscleC,y = meanmuscleN)) + 
   #note x is carbon and y = nitrogen, just like isoscapes, which we are in the process of making here
   geom_point(color = "#00529c") + 
   geom_errorbar(aes(ymin = sdmuscleN,ymax = sdmuscleN)) + 
-  geom_errorbarh(aes(xmin = sdmuscleC,xmax = sdmuscleC)) + 
+  geom_errorbarh(aes(xmin = sdmuscleC,xmax = sdmuscleC))  
   #you will copy and paste all the above code and do it with scale and heart, too, and change the color. you can choose colors by searching hex color pallete on google and entering the numeric hex code in "". 
 
 #The most critical thing to do before any modeling is to check if our data are normally distributed. We can visualiz the raw data, and for best practices, we then visualize the residuals of the data
 
-histogram(lion$muscle_c)
-plotResiduals(lion$muscle_c)
+hist(liondata$muscle_c)
+plotResiduals(liondata$muscle_c)
 #lets check out the distribution of muscle carbon
-normalitymuscle_c<-glm(muscle_c~depth, data = lion)
+normalitymuscle_c<-glm(muscle_c~depth.categorical., data = liondata)
 resmuscle_c<-simulateResiduals(fittedModel = normalitymuscle_c, n = 250)
 plot(resmuscle_c)
 resmuscle_c$scaledResiduals
 testUniformity(resmuscle_c)
+
+#muscle_c is normal (Kolmogorov-Smirnov, p=0.1368)
 
 
