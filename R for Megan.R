9df35b072429003780467fd22490b8154deb4c2e
meganewing <66333757+meganewing@users.noreply.github.com>
2021-07-05 18:20:04 -0700

    modified code as directed by line 75 added lines for nitrogen, scale, and heart ran through script with updated datasheet

--- R for Megan.R
+++ R for Megan.R
@@ -22,12 +22,12 @@ library(lme4)
 
 
 #Okay, lets read in the files and packages we will use for data analysis. If the files is stored in your R project, all you need is a read.csv command! below you will see I am calling in the csv called lionfish data and from here out I'm calling this datasheet/dataframe "lion"
-lion<-read.csv("0702-lionfishdata.csv", header=TRUE)
-<<<<<<< HEAD
+lion<-read.csv("0705-lionfishdata.csv", header=TRUE)
+
 #could you please go back to the original datasheet you just read in and remove any extra spaces inthe variable names. Then remove the extra periods thruout the sheet. Then read in the file again.
-=======
+
 #could you please go back to the original datasheet you just read in and remove any extra spaces inthe variable names. Then remove the extra periods thruout the sheet.
->>>>>>> 356da277b9c35867a5d8c57ae9acd0abb520cd20
+
 #also please make sure the depth for each row is filled in or left blank if unknown.
 
 
@@ -48,14 +48,15 @@ standards<-lion%>%
     meansalmonC = mean(muscle_c, na.rm = TRUE),
     sdsamlmonC= sd(muscle_c, na.rm = TRUE),
     meansalmonN = mean(muscle_n, na.rm = TRUE),
-    sdsamlmonN= sd(muscle_n, na.rm = TRUE))
+    sdsamlmonN= sd(muscle_n, na.rm = TRUE)
+    )
   
 #For our analyses we don't want to include salmon standards or duplicates, so we are going to remove them from the dataframe, and then proceed for the mean and sd for M, S, H, G for C and N
 liondata<-lion %>% 
   filter(calibration == "single" |calibration == "average")
 
 descriptivestats<-liondata %>% 
-  group_by(depth.categorical.)%>% 
+  group_by(depth_categorical)%>% 
   summarize(
     meanmuscleC = mean(muscle_c,na.rm=TRUE),
     sdmuscleC = sd(muscle_c, na.rm = TRUE),
@@ -68,43 +69,102 @@ descriptivestats<-liondata %>%
     meanscaleN = mean(scale_n,na.rm=TRUE),
     sdscaleN = sd(scale_n, na.rm = TRUE),
     meanheartN = mean(heart_n,na.rm=TRUE),
-    sdheartB = sd(heart_n, na.rm = TRUE))
+    sdheartN = sd(heart_n, na.rm = TRUE))
 
 
 #lets visualize our data by making a figure using the new dataframe we just created
 #Megan try followingmodifying the code below as well as taking direction from this website: https://gist.github.com/AndrewLJackson/b16fb216d8a9a96c20a4a979ec97e7b0
-m<-ggplot(data = descriptivestats, aes(x = meanmuscleC,y = meanmuscleN), color = depth.categorical.) + 
+m<-ggplot(data = descriptivestats, aes(x = meanmuscleC,y = meanmuscleN), color = depth_categorical) + 
   #note x is carbon and y = nitrogen, just like isoscapes, which we are in the process of making here
   geom_point(color = "#00529c") + 
   geom_errorbar(aes(ymin = (meanmuscleN-sdmuscleN),ymax = (meanmuscleN + sdmuscleN))) + 
   geom_errorbarh(aes(xmin = (meanmuscleC-sdmuscleC),xmax = (meanmuscleC +sdmuscleC))) 
 
-ms<- musclefig +
+ms<- m + geom_point(color = "#32a889") + 
+  geom_errorbar(aes(ymin = (meanscaleN-sdscaleN),ymax = (meanscaleN + sdscaleN))) + 
+  geom_errorbarh(aes(xmin = (meanscaleC-sdscaleC),xmax = (meanscaleC +sdscaleC))) 
   
+msh<- ms +  geom_point(color = "#a8323c") + 
+  geom_errorbar(aes(ymin = (meanheartN-sdheartN),ymax = (meanheartN + sdheartN))) + 
+  geom_errorbarh(aes(xmin = (meanheartC-sdheartC),xmax = (meanheartC +sdheartC)))  
   
-  
-msh<- ms +   
+print(msh)  
   
   #you will copy and paste all the above code and do it with scale and heart, too, and change the color. you can choose colors by searching hex color pallete on google and entering the numeric hex code in "". 
 
 #The most critical thing to do before any modeling is to check if our data are normally distributed. We can visualiz the raw data, and for best practices, we then visualize the residuals of the data
 
+#CARBON
+
 hist(liondata$muscle_c)
 plotResiduals(liondata$muscle_c)
 #lets check out the distribution of muscle carbon
-normalitymuscle_c<-glm(muscle_c~depth.categorical., data = liondata)
+normalitymuscle_c<-glm(muscle_c~depth_categorical, data = liondata)
 resmuscle_c<-simulateResiduals(fittedModel = normalitymuscle_c, n = 250)
 plot(resmuscle_c)
 resmuscle_c$scaledResiduals
 testUniformity(resmuscle_c)
 
-#muscle_c is normal (Kolmogorov-Smirnov, p=0.1368)
+hist(liondata$scale_c)
+plotResiduals(liondata$scale_c)
+#lets check out the distribution of scale carbon
+normalityscale_c<-glm(scale_c~depth_categorical, data = liondata)
+resscale_c<-simulateResiduals(fittedModel = normalityscale_c, n = 250)
+plot(resscale_c)
+resscale_c$scaledResiduals
+testUniformity(resscale_c)
+
+hist(liondata$heart_c)
+plotResiduals(liondata$heart_c)
+#lets check out the distribution of heart carbon
+normalityheart_c<-glm(heart_c~depth_categorical, data = liondata)
+resheart_c<-simulateResiduals(fittedModel = normalityheart_c, n = 250)
+plot(resheart_c)
+resheart_c$scaledResiduals
+testUniformity(resheart_c)
+
+#NITROGEN
+
+hist(liondata$muscle_n)
+plotResiduals(liondata$muscle_n)
+#lets check out the distribution of muscle nitrogen
+normalitymuscle_n<-glm(muscle_n~depth_categorical, data = liondata)
+resmuscle_n<-simulateResiduals(fittedModel = normalitymuscle_n, n = 250)
+plot(resmuscle_n)
+resmuscle_n$scaledResiduals
+testUniformity(resmuscle_n)
+
+hist(liondata$scale_n)
+plotResiduals(liondata$scale_n)
+#lets check out the distribution of scale nitrogen
+normalityscale_n<-glm(scale_n~depth_categorical, data = liondata)
+resscale_n<-simulateResiduals(fittedModel = normalityscale_n, n = 250)
+plot(resscale_n)
+resscale_n$scaledResiduals
+testUniformity(resscale_n)
+
+hist(liondata$heart_n)
+plotResiduals(liondata$heart_n)
+#lets check out the distribution of heart nitrogen
+normalityheart_n<-glm(heart_n~depth_categorical, data = liondata)
+resheart_n<-simulateResiduals(fittedModel = normalityheart_n, n = 250)
+plot(resheart_n)
+resheart_n$scaledResiduals
+testUniformity(resheart_n)
 
 #How does depth influence the diet of lionfish (using muscle tissue)? This format is response variable ~ indp factor + indp factor + indfactor interacts with other indp factor. We are using SL.mm*depth.categorical bc its likely at shallow depths there is more culling and so we might only find smaller/younger lionfish.  Repeat this formatting for scale and heart. 
 
-mcresults<-lm(muscle_c ~ SL..mm. + depth.categorical. + SL..mm.*depth.categorical., data = liondata)
+mcresults<-lm(muscle_c ~ SL_mm + depth_categorical + SL_mm*depth_categorical, data = liondata)
 summary(mcresults)
-mnresults<-lm(muscle_n ~ SL..mm. + depth.categorical. + SL..mm.*depth.categorical., data = liondata)
+mnresults<-lm(muscle_n ~ SL_mm + depth_categorical + SL_mm*depth_categorical, data = liondata)
 summary(mnresults)
 
+scresults<-lm(scale_c ~ SL_mm + depth_categorical + SL_mm*depth_categorical, data = liondata)
+summary(scresults)
+snresults<-lm(scale_n ~ SL_mm + depth_categorical + SL_mm*depth_categorical, data = liondata)
+summary(snresults)
 
+hcresults<-lm(heart_c ~ SL_mm + depth_categorical + SL_mm*depth_categorical, data = liondata)
+summary(hcresults)
+hnresults<-lm(heart_n ~ SL_mm + depth_categorical + SL_mm*depth_categorical, data = liondata)
+summary(hnresults)
